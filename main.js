7e99853c9b22d9f6f031dbb43c73781971500543
Piotr <piotr.romatowski@gmail.com>
2020-01-28 22:57:28 +0100

    js-wieksza-liczba

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var num1 = 2;
 var num2 = 5;
 
 let mnozenie = function multiply(num1,num2) {
-    return num1*num2;
+    let result = num1*num2
+    return result;
 };
 
 console.log(mnozenie(num1,num2));
