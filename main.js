9b04756c3b85bddc7b2921d7afc4c910fd3100cd
Piotr <piotr.romatowski@gmail.com>
2020-02-05 17:10:51 +0100

    js-wieksza-liczba

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var num1 = 2;
 var num2 = 5;
 
-let mnozenie = function multiply(num1,num2) {
-    let result = num1*num2
-    return result;
-};
 
-console.log(mnozenie(num1,num2));
+if (num1 > num2) {
+    console.log(num1)
+} else if (num2 > num1) {
+    console.log(num2)
+}
+
