1c771aa5ba0765051de0f0d505fd7badd8cef5b3
Piotr <piotr.romatowski@gmail.com>
2020-02-07 18:47:52 +0100

    js-wieksza-liczba

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var num1 = 2;
 var num2 = 5;
 
-
-if (num1 > num2) {
+let liczba = function ( num1, num2 ) {
+    if (num1 > num2) {
     console.log(num1)
 } else if (num2 > num1) {
     console.log(num2)
 }
+}
 
+console.log(liczba(num1,num2))
